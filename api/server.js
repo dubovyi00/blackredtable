883bcb3caa70854fb8efbe55cd9d598b6302a1eb
Egor Demidovich <e.demidovich@trueengineering.ru>
2022-11-22 02:29:41 +0700

    api bones

--- api/server.js
+++ api/server.js
@@ -12,7 +12,37 @@ var jsonParser = bodyParser.json({limit: '10mb'})
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//вход
 app.post('/auth', function(req, res) {
 	console.log("auth", req.body)
+    // req.body.login req.body.pass
     res.send("hi, " + req.body.login)
+})
+
+//регистрация
+app.post('/register', function(req, res) {
+	console.log("register", req.body)
+    // req.body.login req.body.pass
+    res.send("okay, " + req.body.login)
+})
+
+//сохранение результата
+app.post('/submit', function(req, res) {
+	console.log("submit", req.body)
+    // req.body.login req.body.result
+    res.send("submit, " + req.body.login)
+})
+
+//получение рейтинга
+app.get('/scores', function(req, res) {
+	console.log("scores", req.body)
+    // res.body.login res.body.result res.body.timestamp
+    res.send("submit, " + res.body.login)
+})
+
+//получение рейтинга
+app.get('/scores_xls', function(req, res) {
+	console.log("scores_xls", req.body)
+    // res.body.login res.body.result res.body.timestamp
+    res.send("submit, " + res.body.login)
 })
